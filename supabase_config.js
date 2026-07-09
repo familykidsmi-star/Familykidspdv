84123777b48f7340406f3cca3ccb76370137b06d
familykidsmi-star <familykidsmi@gmail.com>
2026-07-09 18:52:20 -0300

    Delete supabase_config.js

--- supabase_config.js
+++
@@ -1,7 +0,0 @@
-// FamilyKids ERP - Configuração do Supabase
-// Projeto paralelo FamilykidsPDV
-// Cole aqui a Publishable Key do Supabase novo.
-window.FK_SUPABASE = {
-  url: "https://rrnoknxqnygfdwyunsin.supabase.co",
-  anonKey: "sb_publishable_yHw7wJKUrZF6uqLsoVX9Gg_mnFHOlAy"
-};
